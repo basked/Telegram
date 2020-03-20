29d7f68da040c9c32302bab8cda50a09152b4ea8
gt-asup6 <basket.baza@gmail.com>
2020-03-20 12:18:19 +0300

    7. Action menu in settings fragment

--- app/src/main/java/pro/basked/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/pro/basked/telegram/ui/fragments/SettingsFragment.kt
@@ -1,12 +1,19 @@
 package pro.basked.telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import pro.basked.telegram.R
+
 
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+      activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)
+    }
 }
